be21a0e7da3d6513348c77a1b2a80e01ee4c3bf5
carlosp2001 <pinedalberto@hotmail.com>
2023-11-27 09:17:25 -0600

    Importancia de hacer logging con Spring Boot

--- todoapp/firstwebapp/src/main/java/com/example/firstwebapp/login/LoginController.java
+++ todoapp/firstwebapp/src/main/java/com/example/firstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.firstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
     // Model se utiliza para transferir información entre el controlador y la vista
-    @RequestMapping("login")
+     @RequestMapping("login")
     public String login(@RequestParam String name, ModelMap model) { // Esta es la forma de obtener un parámetro por medio del query
         model.put("name", name);
-        System.out.println("Request param is: " + name); // No se recomienda utilizar System.out para entorno de
+//        System.out.println("Request param is: " + name); // No se recomienda utilizar System.out para entorno de
+        logger.info("Request param is {}", name);
         // producción
         return "login";
     }
