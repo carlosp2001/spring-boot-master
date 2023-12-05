f077e0a8bdce9b8f6181251ee51d4f2428c95f05
Carlos Pineda <pinedalberto@hotmail.com>
2023-12-05 11:31:49 -0600

    Mejorando asserts para Sping Boot Mock MVC Unit Test

--- first-rest-api/src/test/java/com/example/firstrestapi/survey/SurveyResourceTest.java
+++ first-rest-api/src/test/java/com/example/firstrestapi/survey/SurveyResourceTest.java
@@ -1,6 +1,7 @@
 package com.example.firstrestapi.survey;
 
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -9,6 +10,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 // SurveyResource
 @WebMvcTest(controllers = SurveyResource.class)
@@ -28,11 +34,47 @@ public class SurveyResourceTest {
     private static String SPECIFIC_QUESTION_URL = "http://localhost:8080/surveys/Survey1/questions/Question1";
 
     @Test
-    void retrieveSpecificSurveyQuestion_basicScenario() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL).accept(MediaType.APPLICATION_JSON);
+    void retrieveSpecificQuestion_404Scenario() throws Exception {
+        RequestBuilder requestBuilder =
+                MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL).accept(MediaType.APPLICATION_JSON);
+
         MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
 
+        assertEquals(404, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    void retrieveSpecificSurveyQuestion_basicScenario() throws Exception {
+        RequestBuilder requestBuilder =
+                MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL).accept(MediaType.APPLICATION_JSON);
+
+        Question question = new Question("Question1",
+                "Most Popular Cloud Platform Today", Arrays.asList(
+                "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
+
+        when(surveyService.retrieveSpecificSurveyQuestion("Survey1", "Question1"))
+                .thenReturn(question);
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+
+        String expectedResponse = """
+                {
+                    "id": "Question1",
+                    "description": "Most Popular Cloud Platform Today",
+                    "options": [
+                    "AWS",
+                    "Azure",
+                    "Google Cloud",
+                    "Oracle Cloud"
+                    ],
+                    "correctAnswer": "AWS"
+                }
+                """;
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+
         System.out.println(mvcResult.getResponse().getContentAsString());
-        System.out.println(mvcResult.getResponse().getStatus());
+        JSONAssert.assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString(), false);
+
     }
 }
