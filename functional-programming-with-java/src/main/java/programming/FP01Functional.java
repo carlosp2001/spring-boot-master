11708ebf6af54724b51b139439c930e08f4e8bf4
Carlos Pineda <pinedalberto@hotmail.com>
2023-12-06 11:16:30 -0600

    Usando expresiones lambda para mejorar tu programación funcional

--- functional-programming-with-java/src/main/java/programming/FP01Functional.java
+++ functional-programming-with-java/src/main/java/programming/FP01Functional.java
@@ -7,12 +7,20 @@ public class FP01Functional {
         printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
     }
 
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
     private static void print(int number) {
         System.out.println(number);
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
         // ¿Qué debemos hacer?
-        numbers.stream().forEach(System.out::println); // Method reference
+        numbers.stream()
+                // Filter - Only Allow Even Numbers
+                //.filter(FP01Functional::isEven) // Filter only allow even numbers
+                .filter(number -> number % 2 == 0) // Lambda es una forma simple de definir un método
+                .forEach(System.out::println); // Method reference
     }
 }
